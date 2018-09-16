9f82da4c25518979ff9c2dcdc2b7f9d7d8a2a8e8
icardoso88 <ivancardoso@hotmail.com.ar>
2018-09-16 18:34:11 -0300

    probando repositorio de actividad

---
+++ src/primerprograma/PrimerPrograma.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerprograma;
+
+/**
+ *
+ * @author usuario
+ */
+//Clase Principal
+public class PrimerPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Metodo Main
+    public static void main(String[] args) {
+        // Todo el codigo a ejecutar va dentro de este metodo
+        
+        System.out.println("Hola Mundo");
+        System.out.println("Apellido: CARDOSO");
+        System.out.println("Nombre: IVAN");
+        System.out.println("Profesion: TECNICO EN ADMINISTRACION DE EMPRESAS");
+        System.out.println("Oficio: EMPLEADO");
+        System.out.println("Hobie: JUGAR AL TENIS");
+        System.out.println("Objetivo a corto plazo: APRENDER A CONDUCIR");
+        System.out.println("Objetivo a largo plazo: TENER CASA PROPIA");
+        
+    }
+    
+}
